c049d2f2b611bb76dfe65b572a09c1c5736eb2b8
yangshanghang <1046976621@qq.com>
2018-01-08 10:19:42 +0800

    增加spring管理下的测试类

---
+++ src/test/java/com/ysh/wechat/wxsdk/user/demoTest.java
@@ -0,0 +1,23 @@
+package com.ysh.wechat.wxsdk.user;
+
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+/**
+ * 描述: 测试类 .<br>
+ *
+ * @author YangShangHang
+ * @date 2018/1/8
+ */
+@ContextConfiguration(locations = {"classpath:entry/dev/spring-entry.xml"})
+public class demoTest extends AbstractJUnit4SpringContextTests {
+
+    @Test
+//    @Ignore
+    public void test() throws Exception{
+
+
+    }
+
+}
